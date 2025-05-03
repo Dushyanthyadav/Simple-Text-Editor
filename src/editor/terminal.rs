d5516bdccb3dc6beb10be5f6e43cbc9a68dfcae3
Dushyanth Yadav v <dushyanthyadav6@gmail.com>
2025-05-03 06:38:56 +0530

    Added Better rendering

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -5,7 +5,7 @@ use crossterm::{queue, Command};
 use std::io::{stdout, Error, Write};
 
 
-#[derive(Copy, Clone)]
+#[derive(Default, Copy, Clone)]
 pub struct Size {
     pub height: usize,
     pub width: usize,
